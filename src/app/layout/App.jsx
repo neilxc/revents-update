6163c4e390c11a9eeb73b5b9b3f652f52818349b
trycatchlearn <trycatchlearn@outlook.com>
2018-09-30 11:44:41 +0100

    End of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,15 +3,33 @@ import './App.css';
 import {Container} from "semantic-ui-react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import {Route, Switch} from "react-router-dom";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventForm from "../../features/event/EventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar/>
-        <Container className="main">
-            <EventDashboard/>
-        </Container>
+          <Switch>
+              <Route exact path='/' component={HomePage}/>
+          </Switch>
+          <Route path='/(.+)' render={() => (
+              <Fragment>
+                  <NavBar/>
+                  <Container className="main">
+                      <Route path='/events' component={EventDashboard}/>
+                      <Route path='/event/:id' component={EventDetailedPage}/>
+                      <Route path='/people' component={PeopleDashboard}/>
+                      <Route path='/profile/:id' component={EventDashboard}/>
+                      <Route path='/settings' component={SettingsDashboard}/>
+                      <Route path='/createEvent' component={EventForm}/>
+                  </Container>
+              </Fragment>
+          )}/>
       </Fragment>
     );
   }
